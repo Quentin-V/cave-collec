0cd701c3fc04b2ddd8306675ecf35512011005a3
Quentin Vauthier <quentin.vauthier000@gmail.com>
2022-01-03 15:05:03 +0100

    Added user routes to the server

--- server.js
+++ server.js
@@ -9,9 +9,10 @@ const morgan = require('morgan')
 const http = require('http');
 const https = require('https');
 
-const mangaRoutes = require('./routes/api/manga.js')
-const readRoutes = require('./routes/api/read.js')
+const mangaRoutes   = require('./routes/api/manga.js')
+const readRoutes    = require('./routes/api/read.js')
 const editionRoutes = require('./routes/api/edition.js')
+const userRoutes    = require('./routes/api/user.js')
 
 const certificate = fs.readFileSync('./sslcert/cert.pem')
 const privateKey = fs.readFileSync('./sslcert/privkey.pem')
@@ -29,6 +30,7 @@ mongoose.connect(mongoUri, {
 app.use('/api/manga', mangaRoutes)
 app.use('/api/read', readRoutes)
 app.use('/api/edition', editionRoutes)
+app.use('/api/user', userRoutes)
 
 app.get('/', (req, res) => res.send('hello world'))
 
